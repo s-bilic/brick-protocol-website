e583481d861d4ad0e780e277f2d42693ebbebb27
s-bilic <stefanbilic@hotmail.com>
2023-08-02 23:40:52 +0200

    Footer component

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import classNames from "classnames/bind";
+import styles from "./footer.module.scss";
+import { Icon } from "@ui";
+const cx = classNames.bind(styles);
+
+interface IProps {
+  className?: string;
+  items?: [];
+  socials?: [];
+}
+
+const Footer = ({ className, items, socials }: IProps) => {
+  const classes = cx({ footer: true }, className);
+  return (
+    <footer className={classes}>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <Icon name={"logo"} width={60} height={60} />
+          <h6>Brick Protocol</h6>
+        </div>
+
+        {socials?.map((item, index) => (
+          <Icon key={index} width={20} {...item} />
+        ))}
+        <div className={styles.items}>
+          {items?.map((item, index) => (
+            <div key={index}>
+              {item?.title && <h6 className={styles.title}>{item?.title}</h6>}
+              {item?.column && (
+                <div className={styles.column}>
+                  {item?.column?.map((column, i) => (
+                    <Link key={i} href={column?.url} className={styles.link}>
+                      {column?.link}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/components/index.ts
+++ app/components/index.ts
@@ -2,5 +2,5 @@ import Hero from "./hero/hero";
 import Steps from "./steps/steps";
 import Cta from "./cta/cta";
 import Cards from "./cards/cards";
-
-export { Hero, Steps, Cta, Cards };
+import Footer from "./footer/footer";
+export { Hero, Steps, Cta, Cards, Footer };
